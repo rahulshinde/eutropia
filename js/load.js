a87e8a9b77fbf7555c17cd99ae7440d3cfdf567f
rahul shinde <rshinde@mica.edu>
2015-12-24 01:21:12 -0700

    adding in all html pages

---
+++ js/load.js
@@ -0,0 +1,59 @@
+$(document).ready( function() {
+	$(".content").load("trade.html");
+
+
+    $("#trade").on("click", function() {
+        $(".content").load("trade.html");
+        $('.section').removeClass('visible');
+        $('#trade').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+    $("#id1").on("click", function() {
+        $(".content").load("id1.html");
+        $('.section').removeClass('visible');
+        $('#id1').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+    $("#id2").on("click", function() {
+        $(".content").load("id2.html");
+        $('.section').removeClass('visible');
+        $('#id2').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+    $("#id3").on("click", function() {
+        $(".content").load("id3.html");
+        $('.section').removeClass('visible');
+        $('#id3').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+    $("#app1").on("click", function() {
+        $(".content").load("app1.html");
+        $('.section').removeClass('visible');
+        $('#app1').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+    $("#app2").on("click", function() {
+        $(".content").load("app2.html");
+        $('.section').removeClass('visible');
+        $('#app2').addClass('visible');
+
+        //scroll
+        $('body,html').animate({scrollTop: 0 ,}, 0);
+    });
+
+});
